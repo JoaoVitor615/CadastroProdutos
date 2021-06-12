11ad91fffda7cced493aa1305823a867b44f0329
João Vítor <joao.peniza@etec.sp.gov.br>
2021-06-12 13:10:20 -0300

    Add project files.

---
+++ CadastroProdutos/Models/Produtos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CadastroProdutos.Models
+{
+    public class Produtos
+    {
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        public string Nome { get; set; }
+
+        [StringLength(60, MinimumLength = 5)]
+        public string Descricao { get; set; }
+
+        [Range(0.01, 14.00, ErrorMessage = "Peso Obrigatório entre 0,01kg e 14kg")]
+        public double Peso { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        public string Categoria { get; set; }
+
+        [Range(5.50, 100.00, ErrorMessage = "Valor obrigatório entre R$ 5,50 e R$ 100,00")]
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        public double Vunit { get; set; }
+    }
+}
