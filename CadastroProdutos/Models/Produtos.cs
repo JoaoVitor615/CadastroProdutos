10049a3828d66e2dd35b7fcd42b538286512ac01
João Vítor <joao.peniza@etec.sp.gov.br>
2021-06-12 13:36:00 -0300

    Alterando valores da Model Produtos.cs

--- CadastroProdutos/Models/Produtos.cs
+++ CadastroProdutos/Models/Produtos.cs
@@ -18,13 +18,13 @@ namespace CadastroProdutos.Models
         [StringLength(60, MinimumLength = 5)]
         public string Descricao { get; set; }
 
-        [Range(0.01, 14.00, ErrorMessage = "Peso Obrigatório entre 0,01kg e 14kg")]
+        [Range(0.01, 15.00, ErrorMessage = "Peso Obrigatório entre 0,01kg e 14kg")]
         public decimal Peso { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório!")]
         public string Categoria { get; set; }
 
-        [Range(5.50, 100.00, ErrorMessage = "Valor obrigatório entre R$ 5,50 e R$ 100,00")]
+        [Range(6.80, 100.00, ErrorMessage = "Valor obrigatório entre R$ 5,50 e R$ 100,00")]
         [Required(ErrorMessage = "Este campo é obrigatório!")]
         public decimal Vunit { get; set; }
     }
